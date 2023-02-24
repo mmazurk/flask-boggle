0ceeac34d3ffe3aa79ad885299e45a5fa4cb4bda
markmazurk <markmazurk@gmail.com>
2023-02-24 12:35:34 -0800

    almost done

--- app.py
+++ app.py
@@ -1,48 +1,58 @@
 from boggle import Boggle
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, request, jsonify, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-boggle_game = Boggle()
-board = boggle_game.make_board()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "wow-a-secret"
 debug = DebugToolbarExtension(app)
 
+boggle_game = Boggle()
+board = boggle_game.make_board()
+
 @app.route("/")
 def boggle_start():
     """ home page with boggle board on it"""
 
+    if session['current-board'] == None:
+        session['current-board'] = board
+    else:
+        board = session['current-board']
+
+    if session['games-played'] == None:
+        session['games-played'] = 0
+    
+    if session['high-score'] == None:
+        session['high-score'] = 0
+    
     return render_template("home.html", board = board)
 
 @app.route("/check", methods=['POST'])
 def check_word():
     """ checks to see if the user provided a valid word that is also on the board"""
+    
+    board = session['current-board']
+    data = request.get_json()
+    word_to_check = data['word'].strip().lower()
+    result = boggle_game.check_valid_word(board, word_to_check)
+    return jsonify({"result": result})
 
-# On the server, take the form value and check if it is a valid word in the dictionary
-# Next, make sure that the word is valid on the board using the check_valid_word function from the boggle.py file.
-# Respond with JSON using the jsonify function from Flask ...
-# Send a JSON response which contains either a dictionary of 
-# {“result”: “ok”}, {“result”: “not-on-board”}, or {“result”: “not-a-word”}, so the front-end can provide slightly different messages depending if the word is valid or not.
+# to test if this is working in curl
+# curl localhost:5000/check -d '{"word": "plant"}' -H 'Content-Type: application/json'
 
+@app.route("/newgame", methods=['POST'])
+def new_game():
+    """ checks the high-score, incrememnts number of games played, and makes a new board"""
+    
+    data = request.get_json()
+    score = data['score']
+    if score > session['high-score']:
+        session['high-score'] = score
+    
+    games = session['games-played']
+    games += 1
+    session['games-played'] = games
 
-# TO DO ... 
-# TO DO ... 
-# TO DO ... 
+    board = boggle_game.make_board()
+    session['current-board'] = board
 
-# Put the board in a session variable so we remember it
-# Using jQuery, take the form value and using axios, make an AJAX request to send it to the server.
-
-# On the front-end, display the response from the backend to notify the user if the word is valid and exists on the board, if the word is invalid, or if the word does not exist at all.
-# The score for a word is equal to its length. If a valid word is guessed, add its score to the total and make sure to display the current score as it changes.
-# You can store the score on the front-end for now: it does not need to persist.
-# Instead of letting the user guess for an infinite amount of time, ensure that a game can only be played for 60 seconds. Once 60 seconds has passed, disable any future guesses.
-
-# Now that you have a functional game, let’s keep track of how many times the user has played as well as their highest score so far. When the game ends, send an AJAX request to the server with the score you have stored on the front-end and increment the number of times you have played on the backend.
-
-# Since you will be sending this data as JSON when you make an axios request, you will see this data come in your Flask app inside of request.json not request.form. Use pdb or IPython to set a breakpoint and see what request.json looks like, it is not the same data structure as request.form.
-
-# Refactoring
-# If you have not already, can you design your front-end in an Object Oriented way?
-# Do your view functions have docstrings that describe what they are doing?
-# Are you handling duplicate words? Make sure if you submit the same word, it does not count twice.
+    return redirect("/")
--- static/app.js
+++ static/app.js
@@ -1,8 +1,72 @@
 
+let totalScore = 0
+let wordsPlayed = 0
+let gameStatus = ""
 
-$("#boggle-button").on("click", function(event) {
+// how to toggle classes
+// how to detect the game is over if I include timer
+// (right now I am doing a button)
+
+$("#boggle-button").on("click", async function(event) {
     event.preventDefault()
-    console.log("You clicked the Boggle Button");
+    // console.log("You clicked the Boggle Button");
+    // console.log("Form value is, ", $('#guess').val())
+    let guessedWord = $('#guess').val() 
+    let result = await getWord(guessedWord)
+    console.log(result.data['result'])
+    let wordStatus = result.data['result']
+
+    $('#word-status').removeClass('invisible')
+
+    if (wordStatus === "ok") {
+      $('#word-status').addClass('text-success')
+      $('#word-status').text(`${guessedWord} is a valid word!`)
+      totalScore = totalScore + guessedWord.length
+      $('#total-score').text("Total Score: " + totalScore.toString())
+    }
+    
+    if (wordStatus === "not-word") {
+      $('#word-status').addClass('text-danger')
+      $('#word-status').text(`${guessedWord} is not a word!`)
+    }
+    
+    if (wordStatus === "not-on-board") {
+      $('#word-status').addClass('text-danger')
+      $('#word-status').text(`${guessedWord} is not on the board!`)
+    }
+    
+    $('#guess').val("")
+    wordsPlayed += 1
+    $('#words-played').text("Words Played: " + wordsPlayed.toString())
+
   });
+
+async function getWord(word) {
+  const response = await axios.post('http://127.0.0.1:5000/check', {word: word})
+  // console.log(response)
+  return response
+  }
+
+function timer() {
+
+  $("<div class='container'><h3>Countdown: <span id='countdown'></span></h2></div>").appendTo('body')
+
+  let setTimer = new Date().getTime() + 60000;
+  let x = setInterval(function () {
+
+    let now = new Date().getTime();
+    let distance = setTimer - now;
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    $("#countdown").text(seconds + " seconds left!");
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      $("#guess").addClass("invisible")
+      $("#boggle-button").addClass("invisible")
+    }
+  }, 1000);
+}
+
 
   
